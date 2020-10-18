e6a6e864dbb56e118fa68d3990f563801a4a6072
akib <akibahmed@iut-dhaka.edu>
2020-10-18 20:14:36 +0600

    #App created

---
+++ config.js
@@ -0,0 +1,10 @@
+
+export default FirebaseKeys = {
+    apiKey: "AIzaSyDGJcodY6dTKi79ee5wVizsCqeXCkOIl2E",
+    authDomain: "socialapp-32ec9.firebaseapp.com",
+    databaseURL: "https://socialapp-32ec9.firebaseio.com",
+    projectId: "socialapp-32ec9",
+    storageBucket: "socialapp-32ec9.appspot.com",
+    messagingSenderId: "609662313097",
+    appId: "1:609662313097:web:b30fa3390bc2e84473dc58"
+}
